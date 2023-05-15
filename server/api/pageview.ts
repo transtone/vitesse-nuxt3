d9857e997652aef62a0de9ab4ae6b909c43ae07b
transtone <zm3345@126.com>
2023-05-15 09:45:52 +0800

    feat: add a await for test bug

--- server/api/pageview.ts
+++ server/api/pageview.ts
@@ -1,6 +1,13 @@
 const startAt = Date.now()
 let count = 0
 
+const a = await new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(1)
+  }, 5000)
+})
+console.log(a)
+
 export default defineEventHandler(() => ({
   pageview: count++,
   startAt,
